be22c039ad726f4c7d31a4e526cf133b03936af5
Odboy <tianjun@odboy.cn>
2025-03-25 21:23:39 +0800

    upd: 编码规范

--- cutejava/cutejava-system/src/main/java/cn/odboy/modules/system/mapper/DeptMapper.java
+++ cutejava/cutejava-system/src/main/java/cn/odboy/modules/system/mapper/DeptMapper.java
@@ -5,17 +5,15 @@ import cn.odboy.model.system.dto.DeptQueryCriteria;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
 import java.util.List;
 import java.util.Set;
 
 @Mapper
 public interface DeptMapper extends BaseMapper<Dept> {
-    List<Dept> findDeptListByPage(@Param("criteria") DeptQueryCriteria criteria);
-    List<Dept> findDeptListByPid(@Param("pid") Long pid);
+    List<Dept> findDept(@Param("criteria") DeptQueryCriteria criteria);
+    List<Dept> findDeptByPid(@Param("pid") Long pid);
     int countDeptByPid(@Param("pid") Long pid);
-    List<Dept> findDeptListByPidIsNull();
-    Set<Dept> findDeptSetByRoleId(@Param("roleId") Long roleId);
+    List<Dept> findDeptByPidIsNull();
+    Set<Dept> findDeptByRoleId(@Param("roleId") Long roleId);
     void updateDeptSubCountById(@Param("count") Integer count, @Param("id") Long id);
 }
