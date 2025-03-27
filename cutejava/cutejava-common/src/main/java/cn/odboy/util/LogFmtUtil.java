cbc3ae5f474782c22a1066cb11c09fcfe479864f
Odboy <tianjun@odboy.cn>
2025-03-27 20:42:44 +0800

    upd: 编码规范

--- cutejava/cutejava-common/src/main/java/cn/odboy/util/LogFmtUtil.java
+++ cutejava/cutejava-common/src/main/java/cn/odboy/util/LogFmtUtil.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package cn.odboy.exception.util;
+package cn.odboy.util;
 
 /**
  * 适配 logger 占位符
