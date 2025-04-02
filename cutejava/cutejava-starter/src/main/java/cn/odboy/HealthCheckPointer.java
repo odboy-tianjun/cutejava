2c531c54d05837e594008b456e52ab1e7d1d5145
Odboy <tianjun@odboy.cn>
2025-04-02 20:13:09 +0800

    feat: 健康检查适配kubernetes

---
+++ cutejava/cutejava-starter/src/main/java/cn/odboy/HealthCheckPointer.java
@@ -0,0 +1,23 @@
+package cn.odboy;
+
+import cn.odboy.application.system.service.DictService;
+import cn.odboy.monitor.service.HealthCheckPointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCheckPointer implements HealthCheckPointService {
+    @Autowired
+    private DictService dictService;
+
+    @Override
+    public ResponseEntity<?> doReadiness() {
+        return ResponseEntity.ok(null);
+    }
+
+    @Override
+    public ResponseEntity<?> doLiveness() {
+        return ResponseEntity.ok(dictService.getById(1));
+    }
+}
