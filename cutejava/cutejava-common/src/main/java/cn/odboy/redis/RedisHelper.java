7c8d16ca28ef87ec4706c9683d369807a0b9dd14
Odboy <tianjun@odboy.cn>
2025-06-23 13:21:13 +0800

    fix: 优化RedisUtils中对象反序列化逻辑，支持非目标类型转换。close https://gitee.com/elunez/eladmin-mp/issues/IBSMO5

--- cutejava/cutejava-common/src/main/java/cn/odboy/redis/RedisHelper.java
+++ cutejava/cutejava-common/src/main/java/cn/odboy/redis/RedisHelper.java
@@ -1,6 +1,7 @@
 package cn.odboy.redis;
 
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.slf4j.Logger;
@@ -217,7 +218,10 @@ public class RedisHelper {
         if (value == null) {
             return null;
         }
-        if (clazz.isInstance(value)) {
+        // 如果 value 不是目标类型，则尝试将其反序列化为 clazz 类型 by
+        if (!clazz.isInstance(value)) {
+            return JSON.parseObject(value.toString(), clazz);
+        } else if (clazz.isInstance(value)) {
             return clazz.cast(value);
         } else {
             return null;
